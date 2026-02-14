e3ba6ef749041183b34414a9108ab2dae642767c
Taichi-Dev-1222 <taichi.i.dev@gmail.com>
2026-02-14 15:17:52 +0900

    Reset vpc terraform files and remove local state

--- terraform/vpc/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "tfstate-bucket-vpc-202506"
-    key            = "prd/vpc/terraform.tfstate"
-    region         = "ap-northeast-1"   
-    encrypt        = true                   
-  }
-}
