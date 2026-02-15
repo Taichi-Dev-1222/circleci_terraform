2c7b044c91624f29ddd2ff072df75d9e7aa8161e
Taichi-Dev-1222 <taichi.i.dev@gmail.com>
2026-02-15 22:05:29 +0900

    fix: EC2 backend configuration and SSH key variable setup

--- terraform/ec2/backend.tf
+++ terraform/ec2/backend.tf
@@ -1,18 +1,13 @@
 terraform {
-  backend "s3" {
-    bucket         = "tfstate-bucket-vpc-202506"
-    key            = "prd/ec2/terraform.tfstate"
-    region         = "ap-northeast-1"   
-    encrypt        = true                   
-  }
+  backend "s3" {}
 }
 
 data "terraform_remote_state" "main" {
-  backend          = "s3"
-  config           = {
-    bucket           = "tfstate-bucket-vpc-202506"
-    key              = "prd/vpc/terraform.tfstate"
-    region           = "ap-northeast-1" 
-    encrypt          = true
+  backend = "s3"
+  config = {
+    bucket  = "tfstate-bucket-vpc-202506"
+    key     = "vpc/terraform.tfstate"
+    region  = "ap-northeast-1"
+    encrypt = true
   }
 }
